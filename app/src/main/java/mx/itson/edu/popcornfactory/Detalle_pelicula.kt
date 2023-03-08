98ea26c9b92891244dabf38ffd0a128f03930e03
Brayan Zavala <zavala_243@hotmail.com>
2023-03-08 12:23:42 -0700

    Popcorn Factory Complete

--- app/src/main/java/mx/itson/edu/popcornfactory/Detalle_pelicula.kt
+++ app/src/main/java/mx/itson/edu/popcornfactory/Detalle_pelicula.kt
@@ -1,7 +1,9 @@
 package mx.itson.edu.popcornfactory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -16,6 +18,7 @@ class Detalle_pelicula : AppCompatActivity() {
         val iv_pelicula_imagen: ImageView = findViewById(R.id.iv_pelicula_imagen)
         val tv_nombre_pelicula: TextView = findViewById(R.id.tv_nombre_pelicula)
         val tv_pelicula_desc: TextView = findViewById(R.id.tv_pelicula_desc)
+        val seatLeft: TextView = findViewById(R.id.seatLeft)
 
         val bundle = intent.extras
 
@@ -23,6 +26,14 @@ class Detalle_pelicula : AppCompatActivity() {
             iv_pelicula_imagen.setImageResource(bundle.getInt("header"))
             tv_nombre_pelicula.setText(bundle.getString("titulo"))
             tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+            seatLeft.setText(bundle.getInt("numberSeats").toString())
+        }
+
+        var button: Button = findViewById(R.id.buyTickets)
+
+        button.setOnClickListener{
+            var intento = Intent(this,SeatSelection::class.java)
+            this.startActivity(intento)
         }
     }
 }
--- app/src/main/java/mx/itson/edu/popcornfactory/Pelicula.kt
+++ app/src/main/java/mx/itson/edu/popcornfactory/Pelicula.kt
@@ -3,4 +3,5 @@ package mx.itson.edu.popcornfactory
 class Pelicula (var titulo: String,
                 var image: Int,
                 var header: Int,
-                var sinopsis: String)
+                var sinopsis: String,
+                var seats:ArrayList<Cliente>)
